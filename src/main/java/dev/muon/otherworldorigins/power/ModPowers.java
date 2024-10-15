2a7195e7783b5f1f3e7463c93e116ec8a068f9ef
muon-rw <pashah012@gmail.com>
2024-10-15 15:39:10 -0500

    Progress

--- src/main/java/dev/muon/otherworldorigins/power/ModPowers.java
+++ src/main/java/dev/muon/otherworldorigins/power/ModPowers.java
@@ -3,7 +3,6 @@ package dev.muon.otherworldorigins.power;
 import dev.muon.otherworldorigins.OtherworldOrigins;
 import dev.muon.otherworldorigins.power.configuration.InnateAptitudeBonusConfiguration;
 import io.github.edwinmindcraft.apoli.api.configuration.HolderConfiguration;
-import io.github.edwinmindcraft.apoli.api.configuration.NoConfiguration;
 import io.github.edwinmindcraft.apoli.api.power.configuration.ConfiguredEntityAction;
 import io.github.edwinmindcraft.apoli.api.power.factory.PowerFactory;
 import io.github.edwinmindcraft.apoli.api.registry.ApoliRegistries;
@@ -22,7 +21,7 @@ public class ModPowers {
     public static final RegistryObject<PowerFactory<HolderConfiguration<ConfiguredEntityAction<?, ?>>>> ACTION_ON_LEVELUP = POWER_FACTORIES.register("action_on_levelup", ActionOnLevelupPower::new);
     public static final RegistryObject<PowerFactory<TradeDiscountPower.Configuration>> TRADE_DISCOUNT = POWER_FACTORIES.register("trade_discount", TradeDiscountPower::new);
     public static final RegistryObject<PowerFactory<ModifyThirstExhaustionPower.Configuration>> MODIFY_THIRST_EXHAUSTION = POWER_FACTORIES.register("modify_thirst_exhaustion", ModifyThirstExhaustionPower::new);
-    public static final RegistryObject<PowerFactory<NoConfiguration>> PREVENT_TEMPERATURE = POWER_FACTORIES.register("prevent_temperature", PreventTemperaturePower::new);
+    public static final RegistryObject<PowerFactory<PreventTemperaturePower.Configuration>> PREVENT_TEMPERATURE = POWER_FACTORIES.register("prevent_temperature", PreventTemperaturePower::new);
 
     private static <T extends PowerFactory<?>> RegistryObject<T> registerConditional(String name, Supplier<T> factory, String modId) {
         if (ModList.get().isLoaded(modId)) {
