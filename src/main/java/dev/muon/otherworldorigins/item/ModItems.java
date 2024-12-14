f2a5b9a041a12a1fede1b60e506cc6421e33d902
muon-rw <pashah012@gmail.com>
2024-12-14 12:43:35 -0800

    Icon refactor

--- src/main/java/dev/muon/otherworldorigins/item/ModItems.java
+++ src/main/java/dev/muon/otherworldorigins/item/ModItems.java
@@ -12,6 +12,7 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, OtherworldOrigins.MODID);
 
 
+    /** Races + Subraces */
     public static final RegistryObject<Item> BASE_PORTRAIT = ITEMS.register("portrait/base", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> BLACK_PORTRAIT = ITEMS.register("portrait/black", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> BLUE_PORTRAIT = ITEMS.register("portrait/blue", () -> new Item(new Item.Properties()));
@@ -50,6 +51,66 @@ public class ModItems {
     public static final RegistryObject<Item> VARIANT_PORTRAIT = ITEMS.register("portrait/variant", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> WHITE_PORTRAIT = ITEMS.register("portrait/white", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> WOOD_PORTRAIT = ITEMS.register("portrait/wood", () -> new Item(new Item.Properties()));
+
+
+    /** Classes */
+    public static final RegistryObject<Item> ARTIFICER = ITEMS.register("class/artificer", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> BARD = ITEMS.register("class/bard", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> ROGUE = ITEMS.register("class/rogue", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> SORCERER = ITEMS.register("class/sorcerer", () -> new Item(new Item.Properties()));
+
+
+    /** Subclasses */
+    public static final RegistryObject<Item> ALCHEMIST = ITEMS.register("subclass/artificer/alchemist", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> BATTLE_SMITH = ITEMS.register("subclass/artificer/battle_smith", () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> VALOR = ITEMS.register("subclass/bard/valor", () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> LIFE = ITEMS.register("subclass/cleric/life", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> TRICKERY = ITEMS.register("subclass/cleric/trickery", () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> MOON = ITEMS.register("subclass/druid/moon", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> SPORES = ITEMS.register("subclass/druid/spores", () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> FOUR_ELEMENTS = ITEMS.register("subclass/monk/four_elements", () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ANCIENTS = ITEMS.register("subclass/paladin/ancients", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> DEVOTION = ITEMS.register("subclass/paladin/devotion", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> VENGEANCE = ITEMS.register("subclass/paladin/vengeance", () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> GLOOM_STALKER = ITEMS.register("subclass/ranger/gloom_stalker", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> HUNTER = ITEMS.register("subclass/ranger/hunter", () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ASSASSIN = ITEMS.register("subclass/rogue/assassin", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> THIEF = ITEMS.register("subclass/rogue/thief", () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> DRACONIC_BLOODLINE = ITEMS.register("subclass/sorcerer/draconic_bloodline", () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> GREAT_OLD_ONE = ITEMS.register("subclass/warlock/great_old_one", () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ABJURATION = ITEMS.register("subclass/wizard/abjuration", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> CONJURATION = ITEMS.register("subclass/wizard/conjuration", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> EVOCATION = ITEMS.register("subclass/wizard/evocation", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> NECROMANCY = ITEMS.register("subclass/wizard/necromancy", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> TRANSMUTATION = ITEMS.register("subclass/wizard/transmutation", () -> new Item(new Item.Properties()));
+
+    /** Feats */
+    public static final RegistryObject<Item> CHEF = ITEMS.register("feats/chef", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> DUAL_WIELDER = ITEMS.register("feats/dual_wielder", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> MAGE_SLAYER = ITEMS.register("feats/mage_slayer", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> POLEARM_MASTER = ITEMS.register("feats/polearm_master", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> TOUGH = ITEMS.register("feats/tough", () -> new Item(new Item.Properties()));
+
+    /** Cantrips */
+    public static final RegistryObject<Item> GUST = ITEMS.register("cantrips/gust", () -> new Item(new Item.Properties()));
+
+    /** Apts */
+    public static final RegistryObject<Item> BLD = ITEMS.register("apts/bld", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> STR = ITEMS.register("apts/str", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> INT = ITEMS.register("apts/int", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> CONST = ITEMS.register("apts/const", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LCK = ITEMS.register("apts/lck", () -> new Item(new Item.Properties()));
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
